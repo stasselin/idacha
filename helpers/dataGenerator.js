d7f019302f4f72b7a253eb26c46f8137257ed3d6
Stanislav Selin <stasselin@yandex.ru>
2017-09-28 21:05:47 +0300

    API2

--- helpers/dataGenerator.js
+++ helpers/dataGenerator.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 require('../Models/TagData');
 const TagData = mongoose.model('TagData');
 
-
+mongoose.connect('mongodb://Duskrunner:sselenPIMP2011@ds151014.mlab.com:51014/idacha');
 
 
 let saveData = async () => {
